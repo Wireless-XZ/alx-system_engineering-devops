85ab97e4268178a0cbe855f9ea29e3120c4ab827
xz <odufeso1@gmail.com>
2023-03-09 21:04:39 +0100

    Updated file

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,18 @@
-#Enable the user holberton to login and open files without errors
-
-# Increase hard file limit for holberton user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+# Change the limits for the holberton user
+user_limits { 'holberton':
+  limits => {
+    'nofile' => {
+      soft => '50000',
+      hard => '60000',
+    },
+  },
 }
 
-#Increase soft file limit for user holberton
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
-}
+# Set the file permissions for the file you want the holberton user to access
+file { '/path/to/file':
+  ensure  => present,
+  content => "This is the content of the file.\n",
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+}
