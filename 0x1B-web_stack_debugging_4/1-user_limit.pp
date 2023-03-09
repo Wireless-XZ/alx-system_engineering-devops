52cf7238da9ee68699535cc6f6cc72c13484cbad
xz <odufeso1@gmail.com>
2023-03-09 22:26:20 +0100

    Fixing bugs

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,10 @@
-user { 'holberton':
-  ensure     => present,
-  managehome => true,
-  shell      => '/bin/bash',
+# set a new soft and hard value to the limit
+# of max open files for all the system
+
+exec { 'change-max-open-files-hard-limit':
+  command => "/bin/sed -i /etc/security/limits.conf -e 's/hard nofile [0-9]/hard nofile 4000/g'"
 }
 
-file { '/etc/security/limits.d/holberton.conf':
-  ensure  => file,
-  content => "holberton hard nofile 50000\nholberton soft nofile 50000\n",
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
+exec { 'change-max-open-files-soft-limit':
+  command => "/bin/sed -i /etc/security/limits.conf -e 's/soft nofile [0-9]/soft nofile 3000/g'"
 }
