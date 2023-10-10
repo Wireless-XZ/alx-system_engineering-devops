732227a764309473408066b77874eb2fd03395ce
xz <odufeso1@gmail.com>
2023-10-11 00:03:20 +0100

    New updates

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """function that queries the Reddit API and prints the titles
 of the first 10 hot posts listed for a given subreddit."""
+import requests
 
 
 def top_ten(subreddit):
     """function that queries the Reddit API and prints the titles
     of the first 10 hot posts listed for a given subreddit."""
-    import requests
-    import sys
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
